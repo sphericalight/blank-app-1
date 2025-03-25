3a6c33e3d289a999493780658470e627f6fed1fe
sphericalight <paulhinmilton@gmail.com>
2025-03-25 10:41:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # User input
-velocity = st.slider("Velocity (v/c)", 0.0, 0.99, 0.5)
-time = st.number_input("Time (t)", value=1.0)
+v = st.slider("Velocity (v/c)", -0.99, 0.99, 0.6)
+u = st.slider("Velocity (v/c)", -0.99, 0.99, 0.6)
+ct = st.number_input("Time (t)", value=5.0)
 
 # Do Lorentz transformation
 gamma = 1 / np.sqrt(1 - velocity**2)
-x_prime = lambda x, t: gamma * (x - velocity * t)
-ct_prime = lambda x, t: gamma * (t - velocity * x)
+x_prime = lambda x, t: gamma * (x - v * t)
+ct_prime = lambda x, t: gamma * (ct - v * x)
 
 # Plot
 fig, ax = plt.subplots()
