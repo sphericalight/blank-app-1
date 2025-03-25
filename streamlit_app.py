6ea4c2187122add25e1b364cfd69c7d47f11d8e9
sphericalight <paulhinmilton@gmail.com>
2025-03-25 09:34:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,19 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# User input
+velocity = st.slider("Velocity (v/c)", 0.0, 0.99, 0.5)
+time = st.number_input("Time (t)", value=1.0)
+
+# Do Lorentz transformation
+gamma = 1 / np.sqrt(1 - velocity**2)
+x_prime = lambda x, t: gamma * (x - velocity * t)
+ct_prime = lambda x, t: gamma * (t - velocity * x)
+
+# Plot
+fig, ax = plt.subplots()
+ax.set_title("Lorentz Transformation")
+ax.plot([0, x_prime(1, time)], [0, ct_prime(1, time)], label="Transformed Event")
+ax.legend()
+st.pyplot(fig)
